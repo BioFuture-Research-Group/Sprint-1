c7be1899758954b70aeb03aabf0d211450a23b03
mancancode <madebymaniq@live.com>
2019-09-06 16:17:05 +0100

    new update

--- object-detect-recognize.py
+++ object-detect-recognize.py
@@ -1,20 +1,21 @@
 # import needed python parckages 
 import numpy as np
+import sys
 import argparse
 import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
-ap.add_argument("-p", "--prototxt", required=False,
+ap.add_argument("-p", "--prototxt", required=True,
 	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=False,
+ap.add_argument("-m", "--model", required=True,
 	help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
-#CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 	"sofa", "train", "tvmonitor"]
