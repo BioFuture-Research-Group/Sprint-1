37eadfc25f01423b9569de3ed41bb8429b795da3
mancancode <madebymaniq@live.com>
2019-09-06 19:42:40 +0100

    new commit

--- object-detect-recognize.py
+++ object-detect-recognize.py
@@ -11,13 +11,11 @@ ap.add_argument("-p", "--prototxt", required=True,
 	help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
 	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.2,
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 	"sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
@@ -34,7 +32,7 @@ net.setInput(blob)
 detections = net.forward()
 
 
-#for i in np.arange(0, detections.shape[2]):
+for i in np.arange(0, detections.shape[2]):
 	
 	confidence = detections[0, 0, i, 2]
 
