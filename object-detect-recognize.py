1bf251cf6d8f5b953f4ea875cb38afeb4dffad00
mancancode <madebymaniq@live.com>
2019-09-13 15:12:55 +0100

    just change code

--- object-detect-recognize.py
+++ object-detect-recognize.py
@@ -4,16 +4,7 @@ import sys
 import argparse
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
+
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"sofa", "train", "tvmonitor"]
@@ -27,9 +18,7 @@ image = cv2.imread(args["image"])
 (h, w) = image.shape[:2]
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
 
-print("[INFO] computing object detections...")
-net.setInput(blob)
-detections = net.forward()
+
 
 
 for i in np.arange(0, detections.shape[2]):
